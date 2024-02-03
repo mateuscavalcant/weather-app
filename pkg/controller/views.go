02552d0b671c810f29a7f97ff066c700641487df
Mateus <mateuscavalcant7@gmail.com>
2024-02-03 18:47:43 -0300

    Add views

---
+++ pkg/controller/views.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"html/template"
+	"net/http"
+	"weather-app/pkg/model"
+
+	"github.com/gin-gonic/gin"
+)
+
+var tmpl *template.Template
+
+func WeatherAppView(c *gin.Context) {
+
+	tmpl = template.Must(template.ParseFiles("template/weatherapp.html")) 
+
+	data := model.Data {
+        Icon: "",
+		Temperature: "",
+		City: "",
+		Description: "",
+        Message: "",
+        Error: "",
+	}
+    	
+	RenderTemplate(c, data)
+}
+
+func RenderTemplate(c *gin.Context, data model.Data) {
+	// Renderizar o template
+	err := tmpl.Execute(c.Writer, data)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Erro interno do servidor")
+		return
+	}
+}
